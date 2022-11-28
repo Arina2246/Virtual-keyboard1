2c323ff09f188c41e3e319b5ec41623ca893e7b8
Arina2246 <arinanebylova@gmail.com>
2022-11-28 22:04:17 +0100

    feat: add header

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+import Header from "./components/header/header.js";
+var header = new Header();
+header.addHeader();
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import Header from "./components/header/header.js";
+const header = new Header();
+header.addHeader();
